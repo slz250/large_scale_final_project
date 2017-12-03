6062f629ffa96e0e5cc798c586efadfcc84d7e5a
Noah <noah.oberman@gmail.com>
2017-12-03 17:36:16 -0500

    nothing much

--- app/routes.js
+++ app/routes.js
@@ -39,7 +39,7 @@ module.exports = function (app, passport) {
     });
 
     app.post('/registration', function(req,res){
-
+      let firstName = req.body.
       passport.authenticate('local-registration',function(err, user, message) {
           successRedirect : '/:user_id', // redirect to the secure profile section
           failureRedirect : '/registration', // redirect back to the signup page if there is an error
